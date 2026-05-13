8c503af0feb5a09d5b830e86a439591828a3cdad
bartek8b <163062773+bartek8b@users.noreply.github.com>
2026-05-13 18:38:31 +0200

    Add basic tests

---
+++ src/components/Shop/Shop.test.jsx
@@ -0,0 +1,51 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import { describe, it, expect, vi, beforeEach } from 'vitest';
+import { Shop } from './Shop';
+
+globalThis.fetch = vi.fn();
+
+describe('Shop Component', () => {
+  beforeEach(() => {
+    fetch.mockClear();
+  });
+
+  it('displays products after successfull data fetch', async () => {
+    const mockProducts = [
+      {
+        id: 1,
+        title: 'Golden ring',
+        price: 99.99,
+        category: 'jewelery',
+        image: 'https://src.com/img.jpg',
+        description: 'Beautiful ring',
+      },
+    ];
+
+    fetch.mockResolvedValue({
+      status: 200,
+      json: async () => mockProducts,
+    });
+
+    render(<Shop />);
+
+    // Fragile test - to be checked by aria
+    expect(screen.getByText(/Loading/i)).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(screen.getByText('Golden ring')).toBeInTheDocument();
+    });
+
+    expect(screen.getByText('$ 99.99')).toBeInTheDocument();
+    expect(screen.getByAltText('Golden ring')).toBeInTheDocument();
+  });
+
+  it('displays error when fetch failed', async () => {
+    fetch.mockResolvedValue({ status: 500 });
+
+    render(<Shop />);
+
+    await waitFor(() => {
+      expect(screen.getByText(/A network error was encountered/i)).toBeInTheDocument();
+    });
+  });
+});
