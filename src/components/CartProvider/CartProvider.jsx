e8d672187e4db0ec193a7d53d9c05ed136c01316
bartek8b <163062773+bartek8b@users.noreply.github.com>
2026-05-18 17:31:25 +0200

    Modify cart manipulation feuncs to get quantity

--- src/components/CartProvider/CartProvider.jsx
+++ src/components/CartProvider/CartProvider.jsx
@@ -4,12 +4,36 @@ import { CartContext } from './CartContext';
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
-  function addToCart(item) {
-    setCart((c) => [...c, item]);
+  function addToCart(item, quantity = 1) {
+    setCart((currentCart) => {
+      const existingItem = currentCart.find(
+        (cartItem) => cartItem.id === item.id,
+      );
+
+      if (existingItem) {
+        return currentCart.map((cartItem) =>
+          cartItem.id === item.id
+            ? { ...cartItem, quantity: cartItem.quantity + quantity }
+            : cartItem,
+        );
+      }
+
+      return [...currentCart, { ...item, quantity }];
+    });
   }
 
-  function removeFromCart(id) {
-    setCart((c) => c.filter((cartItem) => cartItem.id !== id));
+  function removeFromCart(id, quantity = 1) {
+    setCart((currentCart) => {
+      return currentCart
+        .map((cartItem) => {
+          if (cartItem.id === id) {
+            return { ...cartItem, quantity: cartItem.quantity - quantity };
+          }
+
+          return cartItem;
+        })
+        .filter((cartItem) => cartItem.quantity > 0);
+    });
   }
 
   function clearCart() {
