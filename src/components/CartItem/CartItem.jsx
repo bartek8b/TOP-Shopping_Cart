5e6a3b9b3f0f7fee94ad6ba671b3cfe219812bbd
bartek8b <163062773+bartek8b@users.noreply.github.com>
2026-05-23 11:45:05 +0200

    Add CartItem component

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -2,10 +2,49 @@ import { useCart } from '../CartProvider/useCart';
 import MinusIcon from '../../assets/icons/circle-minus-solid-full.svg?react';
 import PlusIcon from '../../assets/icons/circle-plus-solid-full.svg?react';
 import TrashIcon from '../../assets/icons/trash-can-solid-full.svg?react';
-import RecycleIcon from '../../assets/icons/recycle-solid-full.svg?react';
 
 export const CartItem = ({ product }) => {
-  const { addToCart, removeFromCart, deleteFromCart, clearCart } = useCart();
+  const { addToCart, removeFromCart, deleteFromCart } = useCart();
 
-  return <article></article>;
+  return (
+    <article>
+      <div className="descriptionContainer">
+        <img src={product.image} alt={product.title} />
+        <p>{product.title}</p>
+      </div>
+      <div className="inputContainer">
+        <button
+          type="button"
+          className="decrease"
+          onClick={() => removeFromCart(product, 1)}
+          aria-label={`Decrease quantity of ${product.title}`}
+        >
+          <MinusIcon aria-hidden="true" />
+        </button>
+        <input
+          type="number"
+          value={product.quantity}
+          readOnly
+          min="1"
+          aria-label={`Quantity of ${product.title}`}
+        />
+        <button
+          type="button"
+          className="increase"
+          onClick={() => addToCart(product, 1)}
+          aria-label={`Increase quantity of ${product.title}`}
+        >
+          <PlusIcon aria-hidden="true" />
+        </button>
+        <button
+          type="button"
+          className="delete"
+          onClick={() => deleteFromCart(product.id)}
+          aria-label={`Delete ${product.title} from cart`}
+        >
+          <TrashIcon aria-hidden="true" />
+        </button>
+      </div>
+    </article>
+  );
 };
