89d68debe22eabdc43eb185504c2e29bada1e42f
bartek8b <163062773+bartek8b@users.noreply.github.com>
2026-05-23 13:12:43 +0200

    Add CartItem component and cart checkout flow

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,13 +1,72 @@
+import { useCart } from '../CartProvider/useCart';
+import { CartItem } from '../../components/CartItem/CartItem';
+import RecycleIcon from '../../assets/icons/recycle-solid-full.svg?react';
+import CheckOutIcon from '../../assets/icons/circle-dollar-to-slot-solid-full.svg?react';
+import { useState } from 'react';
+import { Link } from 'react-router';
+
 export const Cart = () => {
+  const { cart, clearCart } = useCart();
+  const [checkOut, setCheckOut] = useState(false);
 
-    return(
-        <div className="splitLayout">
-            <section>
+  const handleCheckOut = () => {
+    setCheckOut(true);
+  };
 
-            </section>
-            <section>
-                
-            </section>
-        </div>
-    )
+  const resetCheckOut = () => {
+    setCheckOut(false);
+  };
+
+  const total = cart.reduce(
+    (sum, article) => sum + article.price * article.quantity,
+    0,
+  );
+
+  if (!checkOut) {
+    return (
+      <div className="splitLayout">
+        <section>
+          {cart.length === 0 ? (
+            <p>The cart is empty</p>
+          ) : (
+            cart.map((product) => (
+              <CartItem key={product.id} product={product} />
+            ))
+          )}
+        </section>
+        <section>
+          <div>
+            <p>Total price: {`$ ${total}`}</p>
+            <button
+              type="button"
+              className="checkout"
+              onClick={handleCheckOut}
+              disabled={cart.length === 0}
+            >
+              Checkout <CheckOutIcon aria-hidden="true" />
+            </button>
+          </div>
+          <button type="button" className="increase" onClick={clearCart}>
+            Delete all
+            <RecycleIcon aria-hidden="true" />
+          </button>
+        </section>
+      </div>
+    );
+  }
+
+  return (
+    <section>
+      <p>Thank you for using demo</p>
+      <Link
+        to="/"
+        onClick={() => {
+          resetCheckOut();
+          clearCart();
+        }}
+      >
+        Go back to home page
+      </Link>
+    </section>
+  );
 };
