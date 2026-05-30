0f0c235fa14d9d3b14dcbad197303c90ce684c62
bartek8b <163062773+bartek8b@users.noreply.github.com>
2026-05-30 02:25:22 +0200

    Change footer component nesting

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,19 +3,24 @@ import styles from './Footer.module.css';
 
 export const Footer = () => {
   return (
-    <footer>
+    <div className={styles.container}>
       Made in{' '}
       <a
         href="https://en.wikipedia.org/wiki/Poland"
         target="_blank"
-        rel="noopener"
+        rel="noopener noreferrer"
       >
         <ReactCountryFlag countryCode="PL" />
       </a>{' '}
       by{' '}
-      <a href="https://github.com/bartek8b" target="_blank" rel="noopener">
+      <a
+        href="https://github.com/bartek8b"
+        target="_blank"
+        rel="noopener"
+        rel="noopener noreferrer"
+      >
         Bartlomiej Balcerzak
       </a>
-    </footer>
+    </div>
   );
 };
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -13,7 +13,9 @@ export const Layout = () => {
         <Outlet />
       </main>
 
-      <Footer />
+      <footer>
+        <Footer />
+      </footer>
     </>
   );
 };
