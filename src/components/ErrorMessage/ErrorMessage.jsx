af30f23f6fac37e931f98e6c675206cc418f4354
bartek8b <163062773+bartek8b@users.noreply.github.com>
2026-05-30 21:47:39 +0200

    Set layout of Error component

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,4 +1,6 @@
 import ErrorIcon from '../../assets/icons/triangle-exclamation-solid-full.svg?react';
+import { NavLink } from 'react-router-dom';
+import styles from './ErrorMessage.module.css';
 
 export const ErrorMessage = ({ type }) => {
   let message = 'Something went wrong. Please try again later.';
@@ -12,9 +14,12 @@ export const ErrorMessage = ({ type }) => {
   }
 
   return (
-    <div>
-      <ErrorIcon aria-hidden="true" />
-      <p>{message}</p>
+    <div className={styles.container}>
+      <ErrorIcon aria-hidden="true" className={styles.errorIcon} />
+      <p className={styles.message}>{message}</p>
+      <NavLink to="/" end className={styles.goHomeLink}>
+        Go to homepage
+      </NavLink>
     </div>
   );
 };
